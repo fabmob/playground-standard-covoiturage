1fa4888a5726859cd6f1788d1e78a9d274c489aa
Pierre Camilleri <pierre.camilleri@multi.coop>
2022-10-05 13:47:16 +0000

    First building blocks of command line tool

--- cmd/stdcov-cli/main.go
+++ cmd/stdcov-cli/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"net/url"
 )
 
 func main() {
-	baseURLStr := "https://json.validator.validata.fr"
-	baseURL, _ := url.Parse(baseURLStr)
+	urlStrPtr := flag.String("url", "", "Base url of the API under test")
+
+	flag.Parse()
+
+	baseURL, _ := url.Parse(*urlStrPtr)
 	client := &http.Client{}
-	fmt.Println(CheckAPIStatus(client, baseURL))
+	fmt.Println(Check(client, baseURL))
 }
 
 // An HTTPGetter can issue HTTP Get requests
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gitlab.com/multi/stdcov-api-test
 
 go 1.19
+
+require github.com/google/go-cmp v0.5.9 // indirect
