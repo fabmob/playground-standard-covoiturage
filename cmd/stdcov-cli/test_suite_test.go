44085a8e07eed7104824f3aa4011dc08db9dca25
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2022-10-20 14:53:34 +0200

    🔵 Mention t.Helper()

--- cmd/stdcov-cli/test_suite_test.go
+++ cmd/stdcov-cli/test_suite_test.go
@@ -7,10 +7,11 @@ import (
 	"testing"
 )
 
-// throwErrorOnTest returns an urlError for every API call and checks:
+//  testErrorOnRequestIsHandled returns an urlError for every API call and checks:
 // - that only one AssertionError is returned
 // - that AssertionError.Unwrap() != nil
-func testThrowErrorOnTest(t *testing.T, f auxTestFun) {
+func testErrorOnRequestIsHandled(t *testing.T, f auxTestFun) {
+	t.Helper()
 	t.Run("API call throws error", func(t *testing.T) {
 		urlError := &url.Error{Op: "", URL: "", Err: errors.New("error")}
 		m := NewMockClientWithError(urlError)
@@ -30,13 +31,12 @@ func testThrowErrorOnTest(t *testing.T, f auxTestFun) {
 }
 
 func TestAPIErrors(t *testing.T) {
-
 	testCases := []auxTestFun{
 		testGetStatus,
 		testGetDriverJourneys,
 	}
 
 	for _, f := range testCases {
-		testThrowErrorOnTest(t, f)
+		testErrorOnRequestIsHandled(t, f)
 	}
 }
