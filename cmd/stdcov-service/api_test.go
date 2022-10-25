c16a1bf601b821e8c0abbcf578c16cd3db9f70a2
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2022-10-25 15:21:35 +0200

    🔴 comply to maximum 'departureRadius' distance

--- cmd/stdcov-service/api_test.go
+++ cmd/stdcov-service/api_test.go
@@ -11,36 +11,93 @@ import (
 )
 
 func TestCreateUser(t *testing.T) {
-	// Setup
-	e := echo.New()
-	request := httptest.NewRequest(
-		http.MethodGet,
-		"https://fabmob.github.io/driver_journeys?departureLat=0&departureLng=0&arrivalLat=0&arrivalLng=0&departureDate=0&timeDelta=900&departureRadius=1&arrivalRadius=1",
-		nil,
-	)
-	request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(request, rec)
+	t.Run("", func(t *testing.T) {
+		// Setup
+		e := echo.New()
+		request := httptest.NewRequest(
+			http.MethodGet,
+			"https://fabmob.github.io/driver_journeys?departureLat=0&departureLng=0&arrivalLat=0&arrivalLng=0&departureDate=0&timeDelta=900&departureRadius=1&arrivalRadius=1",
+			nil,
+		)
+		request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(request, rec)
 
-	mockDB := NewMockDB()
-	mockDB.driverJourneys = []server.DriverJourney{}
-	handler := &StdCovServerImpl{mockDB}
+		mockDB := NewMockDB()
+		mockDB.driverJourneys = []server.DriverJourney{}
+		handler := &StdCovServerImpl{mockDB}
 
-	params := server.GetDriverJourneysParams{}
+		params := server.GetDriverJourneysParams{}
 
-	// Assertions
-	err := handler.GetDriverJourneys(c, params)
-	if err != nil {
-		t.Fail()
-	}
-	response := rec.Result()
-	a := stdcovcli.NewAssertionAccu()
-	stdcovcli.TestGetDriverJourneys(request, response, a)
-	for _, ar := range a.GetAssertionResults() {
-		if err := ar.Unwrap(); err != nil {
-			t.Log(err)
+		// Assertions
+		err := handler.GetDriverJourneys(c, params)
+		if err != nil {
 			t.Fail()
 		}
+		response := rec.Result()
+		a := stdcovcli.NewAssertionAccu()
+		stdcovcli.TestGetDriverJourneys(request, response, a)
+		for _, ar := range a.GetAssertionResults() {
+			if err := ar.Unwrap(); err != nil {
+				t.Log(err)
+				t.Fail()
+			}
 
-	}
+		}
+	})
+
+	t.Run("DepartureRadius", func(t *testing.T) {
+		// Setup
+		e := echo.New()
+		type coords struct {
+			lat float64
+			lon float64
+		}
+		var (
+			/* coordsRef   = coords{46.1604531, -1.2219607} // reference */
+			coords900m  = coords{46.1613442, -1.2103736} // at ~900m from reference
+			coords1100m = coords{46.1613679, -1.2086563} // at ~1100m from reference
+		)
+		request := httptest.NewRequest(
+			http.MethodGet,
+			"https://fabmob.github.io/driver_journeys?departureLat=46.1604531&departureLng=-1.2219607&arrivalLat=0&arrivalLng=0&departureDate=0&timeDelta=900&departureRadius=1&arrivalRadius=1",
+			nil,
+		)
+		request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(request, rec)
+
+		mockDB := NewMockDB()
+		mockDB.driverJourneys = []server.DriverJourney{
+			{
+				PassengerPickupLat: coords900m.lat,
+				PassengerPickupLng: coords900m.lon,
+				Type:               "DYNAMIC",
+			},
+			{
+				PassengerPickupLat: coords1100m.lat,
+				PassengerPickupLng: coords1100m.lon,
+				Type:               "DYNAMIC",
+			},
+		}
+		handler := &StdCovServerImpl{mockDB}
+
+		params := server.GetDriverJourneysParams{}
+
+		// Assertions
+		err := handler.GetDriverJourneys(c, params)
+		if err != nil {
+			t.Fail()
+		}
+		response := rec.Result()
+		a := stdcovcli.NewAssertionAccu()
+		stdcovcli.TestGetDriverJourneys(request, response, a)
+		for _, ar := range a.GetAssertionResults() {
+			if err := ar.Unwrap(); err != nil {
+				t.Log(err)
+				t.Fail()
+			}
+
+		}
+	})
 }
