9d0ea7484ff99f1d323df022cc01403c885c050a
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2022-11-04 15:22:31 +0100

    🔵 never use empty apiMapping

--- cmd/test/test_internals_test.go
+++ cmd/test/test_internals_test.go
@@ -38,7 +38,7 @@ func testErrorOnRequestIsHandled(t *testing.T, f requestTestFun) {
 }
 
 func TestAPIErrors(t *testing.T) {
-	for _, fun := range apiMapping {
+	for _, fun := range GetAPIMapping() {
 		testErrorOnRequestIsHandled(t, wrapTestResponseFun(fun))
 	}
 }
