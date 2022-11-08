0a6690a493bd38d1df5af4abd7ff89ab5db3099c
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2022-11-08 11:03:28 +0100

    🔵 CI command tests on separate script

---
+++ cmd/test/test_commands.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Testing different testing commands. To be run inside the project root
+# directory. The commands are not expected to test any data.
+
+# exit when any command fails
+set -e
+set -o pipefail
+
+echo "Run fake server"
+go run main.go serve > /dev/null &
+sleep 1
+
+echo "Test GET /driver_journeys with url"
+go run main.go test \
+  --url "http://localhost:1323/driver_journeys" \
+  -q departureLat=0 \
+  -q departureLng=0 \
+  -q arrivalLat=0 \
+  -q arrivalLng=0 \
+  -q departureDate=0 \
+  -q timeDelta=0 \
+  -q departureRadius=0 \
+  -q arrivalRadius=0
+
+
+echo "Test GET /passenger_journeys with short command, no optional query parameter"
+go run main.go test get passengerJourneys --server "http://localhost:1323" --departureLat=0 --departureLng=0 --arrivalLat=0 --arrivalLng=0 --departureDate=0
+
+
+echo "Test GET /passenger_journeys with short command, all optional query parameter"
+go run main.go test get passengerJourneys --server "http://localhost:1323" --departureLat=0 --departureLng=0 --arrivalLat=0 --arrivalLng=0 --departureDate=0
+
+# echo "Test GET /bookings/{bookingId} with short command"
+# go run main.go test get bookings --server "http://localhost:1323" --bookingId="42"
+
+# Clean up subprocesses on exit
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
