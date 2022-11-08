655d516206cefbcbedf244cf95b089a87b4af39b
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2022-11-08 12:15:08 +0100

    🔵 remove unnecessary `initAPIMapping()`

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -8,8 +8,6 @@ import (
 // Run runs the cli validation and returns an exit code
 func Run(method, URL string, verbose bool, query Query, flags Flags) int {
 
-	initAPIMapping()
-
 	req, err := http.NewRequest(method, URL, nil)
 	if err != nil {
 		fmt.Println(err)
