a0fbb7dc8a3120204003c63ab5d7a69cf51365af
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2022-11-09 21:43:09 +0100

    🟢 # Veuillez saisir le message de validation pour vos modifications. Les lignes

---
+++ cmd/test/validate_format.go
@@ -0,0 +1,64 @@
+package test
+
+import (
+	"context"
+	"io"
+	"net/http"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/getkin/kin-openapi/openapi3filter"
+	"github.com/getkin/kin-openapi/routers/gorillamux"
+
+	"github.com/fabmob/playground-standard-covoiturage/spec"
+)
+
+// Response validates a Response against the openapi specification.
+func validateResponse(request *http.Request, response *http.Response) error {
+	ctx := context.Background()
+	loader := &openapi3.Loader{Context: ctx, IsExternalRefsAllowed: true}
+
+	doc, loadingErr := loader.LoadFromData(spec.OpenAPISpec)
+	if loadingErr != nil {
+		panic(loadingErr) // Error only if problem with module internals
+	}
+
+	specValidationErr := doc.Validate(ctx)
+	if specValidationErr != nil {
+		panic(specValidationErr) // Error only if problem with module internals
+	}
+
+	router, routerErr := gorillamux.NewRouter(doc)
+	if routerErr != nil {
+		panic(routerErr) // Error only if problem with module internals
+	}
+
+	// Find route
+	route, pathParams, err := router.FindRoute(request)
+	if err != nil {
+		return err
+	}
+
+	requestValidationInput := &openapi3filter.RequestValidationInput{
+		Request:    request,
+		PathParams: pathParams,
+		Route:      route,
+	}
+
+	// Validate response
+	responseValidationInput := &openapi3filter.ResponseValidationInput{
+		RequestValidationInput: requestValidationInput,
+		Status:                 response.StatusCode,
+		Header:                 response.Header,
+		Options:                &openapi3filter.Options{IncludeResponseStatus: true},
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	responseValidationInput.SetBodyBytes(body)
+	validationErr := openapi3filter.ValidateResponse(ctx, responseValidationInput)
+
+	return validationErr
+}
