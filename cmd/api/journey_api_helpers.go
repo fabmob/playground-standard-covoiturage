29f62e922542ad31b77fc6005b474118bfafcc19
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2022-11-25 13:55:04 +0100

    refactor: renaming files and clean up of package api (#44)

--- cmd/api/journey_api_helpers.go
+++ cmd/api/journey_api_helpers.go
@@ -1,6 +1,78 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/fabmob/playground-standard-covoiturage/cmd/util"
+	"github.com/labstack/echo/v4"
+)
+
+// NewGetDriverJourneysParams returns query parameters, looking for a trip
+// from "departure" to "arrival" at "departureDate".
+func NewGetDriverJourneysParams(
+	departure, arrival util.Coord,
+	departureDate int,
+) *GetDriverJourneysParams {
+	return &GetDriverJourneysParams{
+		DepartureLat:  float32(departure.Lat),
+		DepartureLng:  float32(departure.Lon),
+		ArrivalLat:    float32(arrival.Lat),
+		ArrivalLng:    float32(arrival.Lon),
+		DepartureDate: departureDate,
+	}
+}
+
+// NewGetPassengerJourneysParams returns query parameters, looking for a trip
+// from "departure" to "arrival" at "departureDate".
+func NewGetPassengerJourneysParams(
+	departure, arrival util.Coord,
+	departureDate int,
+) *GetPassengerJourneysParams {
+
+	return &GetPassengerJourneysParams{
+		DepartureLat:  float32(departure.Lat),
+		DepartureLng:  float32(departure.Lon),
+		ArrivalLat:    float32(arrival.Lat),
+		ArrivalLng:    float32(arrival.Lon),
+		DepartureDate: departureDate,
+	}
+}
+
+// NewDriverJourney returns a valid DriverJourney
+func NewDriverJourney() DriverJourney {
+	dj := DriverJourney{}
+	dj.Type = "DYNAMIC"
+	dj.Operator = "example.com"
+
+	return dj
+}
+
+// NewPassengerJourney returns a valid PassengerJourney
+func NewPassengerJourney() PassengerJourney {
+	pj := PassengerJourney{}
+	departureDate := int64(0)
+	pj.Operator = "example.com"
+	pj.DriverDepartureDate = &departureDate
+	pj.Type = "DYNAMIC"
+
+	return pj
+}
+
+func GetJourneys(s ServerInterface, ctx echo.Context, params GetJourneysParams) error {
+	switch v := params.(type) {
+	case *GetPassengerJourneysParams:
+		return s.GetPassengerJourneys(ctx, *params.(*GetPassengerJourneysParams))
+
+	case *GetDriverJourneysParams:
+		return s.GetDriverJourneys(ctx, *params.(*GetDriverJourneysParams))
+
+	default:
+		return fmt.Errorf("unknown journey type %v: only GetDriverJourneysParams and GetPassengerJourneys supported", v)
+	}
+}
+
+///////////////////////////////////////////////
 
 type RequestParams interface {
 	MakeRequest(server string) (*http.Request, error)
