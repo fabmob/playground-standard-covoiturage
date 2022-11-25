130d8dfb866e666d6b47f83ad99a5e7ea82e8ccf
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2022-11-25 13:52:45 +0100

    🔵 moving files around, removing unused exported function

--- cmd/api/gogen.go
+++ cmd/api/gogen.go
@@ -2,7 +2,13 @@
 // specification.
 //
 // Most of the functions are generated from the OpenAPI specfication with
-// oapi-codegen
+// oapi-codegen.
+//
+// The package also exports:
+//
+// * helpers to perform conversions between
+// Driver/PassengerCarpoolBooking and Booking types.
+// * umbrella type `Journeys` to manipulate driverJourneys and passengerJourneys alike
 package api
 
 //go:generate oapi-codegen -config oapi-codegen-config.yaml ../../spec/stdcov_openapi.yaml
